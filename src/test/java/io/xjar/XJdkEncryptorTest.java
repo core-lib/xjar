afae50d357479c65310d302b0fc0d001f806e865
core-lib <646742615@qq.com>
2018-11-23 20:12:25 +0800

    优化

--- src/test/java/io/xjar/XJdkEncryptorTest.java
+++ src/test/java/io/xjar/XJdkEncryptorTest.java
@@ -8,14 +8,11 @@ import org.junit.Test;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.util.Random;
-import java.util.zip.CRC32;
-import java.util.zip.CheckedOutputStream;
 import java.util.zip.Deflater;
 
 /**
@@ -23,16 +20,6 @@ import java.util.zip.Deflater;
  * 2018/11/22 14:19
  */
 public class XJdkEncryptorTest {
-
-    @Test
-    public void checksum() throws Exception {
-        File file = new File("G:\\workspace\\regent-service\\regent-service-mr\\regent-service-mr-web\\target\\classes\\application.yml");
-        FileInputStream fis = new FileInputStream(file);
-        CheckedOutputStream cos = new CheckedOutputStream(System.out, new CRC32());
-        XKit.transfer(fis, cos);
-        System.out.println();
-        System.out.println(file.length() + ":" + Integer.toHexString((int) cos.getChecksum().getValue()));
-    }
 
     @Test
     public void test() throws Exception {
